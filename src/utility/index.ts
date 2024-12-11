63a1c0509d976200f9d97089d5b91de2a17f56f9
Coin <coinerartist@gmail.com>
2024-12-11 18:58:13 +0100

    Fixed exports of utility

--- src/utility/index.ts
+++ src/utility/index.ts
@@ -1,9 +1,3 @@
 export * from "./files/input.ts"
 export * from "./files/output.ts"
 export * from "./files/log.ts"
-
-export * from "./classes/grid.ts"
-export * from "./classes/objectset.ts"
-
-export * from "./math.ts"
-export * from "./consts.ts"
