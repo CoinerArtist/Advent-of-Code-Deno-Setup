2c606737a0c016daf4d2838e2e611bbc681c39e1
Coin <coinerartist@gmail.com>
2024-12-22 16:57:21 +0100

    Better log function.

--- src/utility/files/log.ts
+++ src/utility/files/log.ts
@@ -1,13 +1,10 @@
-import { Console } from "node:console";
-import { createWriteStream, WriteStream } from "node:fs"
+const enc = new TextEncoder()
+let files: Record<string, Deno.FsFile> = {}
 
-const consoles: Record<string, [Console, WriteStream]> = {}
-
-export function log(value: any, outFile="console"){
-    if(!consoles[outFile]){ 
-        const stream = createWriteStream(outFile + ".txt")
-        consoles[outFile] = [new Console(stream, stream), stream]
+export function log(value: any, outFile="console", options: Deno.InspectOptions = {}){
+    if(files[outFile] === undefined){
+        files[outFile] = Deno.openSync(outFile + ".txt", {write: true, create: true, truncate: true})
     }
 
-    consoles[outFile][0].log(value)
+    files[outFile].writeSync(enc.encode(Deno.inspect(value, options)))
 }
